55e52e8a62aaff431356f53f7dc7de6481da071e
Edward Pritchard <edward.d.pritchard@gmail.com>
2018-10-17 14:16:35 -0700

    deploy: add avatar
    Added avatars to the repo.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { handleInitialData } from "../actions/shared";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+
   render() {
     return <div>This is the App wrapper</div>;
   }
 }
 
-export default App;
+export default connect()(App);
