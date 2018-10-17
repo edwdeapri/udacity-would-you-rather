7cc7bdec8f2ebbc80ba3f5c9cb95839e9d245f2f
Edward Pritchard <edward.d.pritchard@gmail.com>
2018-10-17 12:24:21 -0700

    deploy: add reducer functionality
    Added reducer functionality to the application. reducers for autheduser, index, questions and users. added dependancies and updated root index file.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 import App from "./components/App";
+import reducer from "./reducers";
 
 import "./styles.css";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
