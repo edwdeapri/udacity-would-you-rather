8047cdef6e5b2f94cf5980961ce93f69e63a321f
Edward Pritchard <edward.d.pritchard@gmail.com>
2018-10-17 13:16:10 -0700

    deploy: add middleware functionality
    Added middleware functionality to the application. middleware for logger and index. added dependancies and updated root index file.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 
 import App from "./components/App";
+import middleware from "./middleware";
 import reducer from "./reducers";
 
 import "./styles.css";
 
-const store = createStore(reducer);
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,4 +1,5 @@
 import { SET_AUTHED_USER } from "../actions/authedUser";
+
 export default function authedUser(state = null, action) {
   switch (action.type) {
     case SET_AUTHED_USER:
